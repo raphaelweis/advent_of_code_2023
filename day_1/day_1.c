d0496ab8b78221677fd91d704f896756a82b9cbc
Raphaël Weis <raphael.weis@outlook.com>
2023-12-07 10:16:00 +0100

    (Fix) Formatted output of day_1 to be cleaner.

--- day_1/day_1.c
+++ day_1/day_1.c
@@ -7,8 +7,8 @@ static char numbers[10][6] = {"one", "two", "three", "four", "five", "six", "sev
 
 int main(const int argc, char** argv) {
     FILE* f = fopen(argv[argc - 1], "r");
-    char c, str[10], num[100];
-    int i = 0, j = 0, sum = 0;
+    char c, str[10], str2[10], num[100];
+    int i = 0, j = 0, k = 0, sum1 = 0, sum2 = 0;
     do {
         c = fgetc(f);
         if (c == '\n') {
@@ -16,18 +16,21 @@ int main(const int argc, char** argv) {
             n[0] = str[0];
             n[1] = str[i - 1];
             n[2] = '\0';
-            sum += atoi(n);
-            i = 0, j = 0;
+            sum1 += atoi(n);
+            n[0] = str2[0];
+            n[1] = str2[j - 1];
+            sum2 += atoi(n);
+            i = 0, j = 0, k = 0;
             continue;
         }
         if (!isdigit(c)) {
-            num[j] = c;
-            j++;
-            for (int k = 0; k < sizeof(numbers) / sizeof(numbers[0]) - 1; k++) {
-                if (strstr(num, numbers[k]) != NULL) {
-                    str[i] = k + 1 + '0';
-                    i++;
-                    j = 1;
+            num[k] = c;
+            k++;
+            for (int l = 0; l < sizeof(numbers) / sizeof(numbers[0]) - 1; l++) {
+                if (strstr(num, numbers[l]) != NULL) {
+                    str2[j] = l + 1 + '0';
+                    j++;
+                    k = 1;
                     memset(num, 0, strlen(num));
                     num[0] = c; // to avoid stuff like oneight not being recognized correctly.
                     break;
@@ -36,8 +39,10 @@ int main(const int argc, char** argv) {
             continue;
         };
         str[i] = c;
-        i++;
+        str2[j] = c;
+        i++, j++;
     } while (c != EOF);
-    printf("Sum: %d\n", sum);
+    printf("Part 1: %d\n", sum1);
+    printf("Part 2: %d\n", sum2);
     return 0;
 }
