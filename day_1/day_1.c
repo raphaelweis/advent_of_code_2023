9a9cc22537df269b216ba5a7b032bc5f751bd205
Raphaël Weis <raphael.weis@outlook.com>
2023-12-07 01:07:33 +0100

    (Add) Finished day 1, rewrote the entire program to be cleaner.

--- day_1/day_1.c
+++ day_1/day_1.c
@@ -1,43 +1,43 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define BUFSIZE 1000
-#define STRSIZE 2
+static char numbers[10][6] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine",};
 
-int main(void) {
-	FILE* filep;
-
-   	filep = fopen("input.txt", "r");
-
-	char strings[1000][3];
-	char c;
-	for (int i = 0; i < 1000; i++) {
-		int j = 0;
-		char tempstr[10];
-		do {
-			c = fgetc(filep);
-			if (isdigit(c)) {
-				tempstr[j] = c;
-				j++;
-			}
-		} while (c != '\n');
-		tempstr[j] = '\0';
-		
-		strings[i][0] = tempstr[0];
-
-		int length = strlen(tempstr) - 1;
-		strings[i][1] = tempstr[length];
-		strings[i][2] = '\0';
-	}
-
-
-	int sum = 0;
-	for (int k = 0; k < 1000; k++) {
-		sum += atoi(strings[k]);
-	}
-
-	printf("%d\n", sum);
-	return 0;
+int main(const int argc, char** argv) {
+    FILE* f = fopen(argv[argc - 1], "r");
+    char c, str[10], num[100];
+    int i = 0, j = 0, sum = 0;
+    do {
+        c = fgetc(f);
+        if (c == '\n') {
+            char n[3];
+            n[0] = str[0];
+            n[1] = str[i - 1];
+            n[2] = '\0';
+            sum += atoi(n);
+            i = 0, j = 0;
+            continue;
+        }
+        if (!isdigit(c)) {
+            num[j] = c;
+            j++;
+            for (int k = 0; k < sizeof(numbers) / sizeof(numbers[0]) - 1; k++) {
+                if (strstr(num, numbers[k]) != NULL) {
+                    str[i] = k + 1 + '0';
+                    i++;
+                    j = 1;
+                    memset(num, 0, strlen(num));
+                    num[0] = c; // to avoid stuff like oneight not being recognized correctly.
+                    break;
+                };
+            }
+            continue;
+        };
+        str[i] = c;
+        i++;
+    } while (c != EOF);
+    printf("Sum: %d\n", sum);
+    return 0;
 }
