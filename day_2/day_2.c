c428a747fdbca0311de3f717cfa9c2c7d784a765
Raphaël Weis <raphael.weis@outlook.com>
2023-12-07 13:09:53 +0100

    (Feat) Day2 part 2.

--- day_2/day_2.c
+++ day_2/day_2.c
@@ -6,17 +6,15 @@
 #define NUM_SIZE 3
 #define COLOR_SIZE 6
 
-static const int totR = 12;
-static const int totG = 13;
-static const int totB = 14;
+static const int totR = 12, totG = 13, totB = 14;
 
 int main(const int argc, char** argv) {
     FILE* f = fopen(argv[argc - 1], "r");
 
     char c;
-    int i = 0, j = 0, id = 1, sum1 = 0, maxR = 0, maxG = 0, maxB = 0;
-    char num[NUM_SIZE];
-    char token[COLOR_SIZE];
+    char num[NUM_SIZE], token[COLOR_SIZE];
+    int i = 0, j = 0, id = 1, sum1 = 0, sum2 = 0, maxR = 0, maxG = 0, maxB = 0;
+
     do {
         c = fgetc(f);
 
@@ -24,6 +22,7 @@ int main(const int argc, char** argv) {
             num[i++] = c;
             continue;
         }
+
         if (c == ':') {
             id = atoi(num);
             i = 0, j = 0;
@@ -31,10 +30,12 @@ int main(const int argc, char** argv) {
             memset(token, 0, COLOR_SIZE * sizeof(char));
             continue;
         }
+
         if (c == ' ') {
             j = 0;
             memset(token, 0, COLOR_SIZE * sizeof(char));
         }
+
         if (strstr(token, "red") != NULL) {
             int const current = atoi(num);
             maxR = current > maxR ? current : maxR;
@@ -56,22 +57,17 @@ int main(const int argc, char** argv) {
             memset(token, 0, COLOR_SIZE * sizeof(char));
             i = 0, j = 0;
         }
+
         if (c == '\n') {
-            printf("Line %d, R = %d, G = %d, B = %d. ", id, maxR, maxG, maxB);
-            if (!(maxR > totR || maxG > totG || maxB > totB)) {
-                sum1 += id;
-                printf(" POSSIBLE\n");
-            } else {
-                printf(" NOT POSSIBLE\n");
-            }
+            sum2 += maxR * maxG * maxB;
+            if (!(maxR > totR || maxG > totG || maxB > totB)) sum1 += id;
             maxR = 0, maxG = 0, maxB = 0;
         }
 
         token[j++] = c;
     } while (c != EOF);
-
     printf("Part 1: %d\n", sum1);
-
+    printf("Part 2: %d\n", sum2);
     fclose(f);
     return 0;
 }
